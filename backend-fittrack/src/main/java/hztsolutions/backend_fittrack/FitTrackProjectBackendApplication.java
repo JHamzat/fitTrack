3f21d9ab468f5fe94a73acf74acc0e9031ebdf8c
JHamzat <40691596+JHamzat@users.noreply.github.com>
2025-09-03 21:31:34 +0100

    Generated base of Spring Boot backend, using Spring Initializr

---
+++ backend-fittrack/src/main/java/hztsolutions/backend_fittrack/FitTrackProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package hztsolutions.backend_fittrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitTrackProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitTrackProjectBackendApplication.class, args);
+	}
+
+}
---
+++ backend-fittrack/src/test/java/hztsolutions/backend_fittrack/FitTrackProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hztsolutions.backend_fittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitTrackProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
